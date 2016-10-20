0090359f7a21b79a057fd7127ae38778a5230f4a
Miklós Koren <miklos.koren@gmail.com>
2016-10-20 18:24:47 +0200

    specs for Table 2a and 3

--- Makefile
+++ Makefile
@@ -4,12 +4,16 @@ MATLAB		= matlab
 MAP_BEGIN	= $(MATLAB) -nodisplay -r "try, run_model('
 MAP_END		= '), catch, exit(1), end, exit(0);"
 TABLE1		= table1_baseline table1_kappa1972 table1_nosectoral table1_kappa1972_nosectoral
+TABLE2a		= table2a_baseline table2a_kappa1972 table2a_nosectoral table2a_kappa1972_nosectoral
+TABLE3		= table3_baseline table3_kappa1972 table3_nosectoral table3_kappa1972_nosectoral
 
-.PHONY: all table1
+.PHONY: all table1 table2a table3 
 
-all: table1
+all: table1 table2a table3 
 
 table1: $(foreach specification,$(TABLE1),models/$(specification)/volatilities.csv)
+table2a: $(foreach specification,$(TABLE2a),models/$(specification)/volatilities.csv)
+table3: $(foreach specification,$(TABLE3),models/$(specification)/volatilities.csv)
 
 models/%/volatilities.csv: model_specifications/%.m 
 	$(MAP_BEGIN)$(notdir $<)$(MAP_END)
---
+++ model_specifications/table2a_kappa1972.m
@@ -0,0 +1,39 @@
+% This file describes a specific model specification for
+%                       include ref here
+
+% There are three main types of parameters that describe a model:
+% 1. Model type (I-O links, Balanced trade, Labor adjustment)
+% 2. Fundamental parameters (theta, eta)
+% 3. Counterfactual parameters (Trade costs, Productivity shocks)
+
+
+
+%% Model type
+% io links (bool)
+spec.iol = 1;
+
+% Balanced trade (bool)
+spec.bt = 0;
+
+% Labor adjustment cost (0: infinity, int: rho)
+spec.lac = 0;
+
+
+
+%% Fundamental parameters
+% theta in {2, 4, 8}
+spec.th = 2;
+
+% eta (int?)
+spec.et = 4;
+
+
+
+%% Conterfactual parameters
+% trade cost (0: actual calibrated, 1: 1972, 2: free trade)
+spec.tc = 1;
+
+% productivity shock type (0: actual calibrated,
+%                          1: no sectoral shocks,
+%                          2: no sectoral and residual shocks)
+spec.sh = 0;
