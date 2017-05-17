283a732a455d870de72aa16d98f27b0751dab447
balazskertesz <balazs_kertesz@yahoo.com>
2017-05-17 11:41:40 +0200

    table3 generator
    Robustness: different thetas	
    Column 1	Difference between full model and case with constant trade costs with theta = 2 
    Column 2	Difference between full model without sectoral shocks, and case with constant trade costs without sectoral shocks, with theta = 2 
    Column 3	Difference between columns 1 and 2
    Column 4	Difference between full model and case with constant trade costs with theta = 8
    Column 5	Difference between full model without sectoral shocks, and case with constant trade costs without sectoral shocks, with theta = 8
    Column 6	Difference between columns 4 and 5

---
+++ table_3.m
@@ -0,0 +1,33 @@
+th = 2;
+
+t1 = ['table_th_', num2str(th), '_lac_inf_bt'];
+
+load_model_specifications(t1)
+
+run_model([t1, '_baseline'])
+
+run_model([t1, '_nosectoral'])
+
+run_model([t1, '_kappa1972'])
+
+run_model([t1, '_kappa1972_nosectoral'])
+
+create_table(t1)
+
+
+
+th = 8;
+
+t1 = ['table_th_', num2str(th), '_lac_inf_bt'];
+
+load_model_specifications(t1)
+
+run_model([t1, '_baseline'])
+
+run_model([t1, '_nosectoral'])
+
+run_model([t1, '_kappa1972'])
+
+run_model([t1, '_kappa1972_nosectoral'])
+
+create_table(t1)
