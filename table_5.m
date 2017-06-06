1c9cfad8d4d99ff9fe5189073ec0fcf37058c0b4
balazskertesz <balazs_kertesz@yahoo.com>
2017-06-06 11:29:18 +0200

    Additional robustness check in LAC

--- table_5.m
+++ table_5.m
@@ -31,3 +31,39 @@ run_model([t1, '_kappa1972'])
 run_model([t1, '_kappa1972_nosectoral'])
 
 create_table(t1)
+
+
+
+th = 4;
+
+t1 = ['table_th_', num2str(th), '_lac_500_bt'];
+
+load_model_specifications(t1)
+
+run_model([t1, '_baseline'])
+
+run_model([t1, '_nosectoral'])
+
+run_model([t1, '_kappa1972'])
+
+run_model([t1, '_kappa1972_nosectoral'])
+
+create_table(t1)
+
+
+
+%th = 4;
+
+%t1 = ['table_th_', num2str(th), '_lac_100_bt'];
+
+%load_model_specifications(t1)
+
+%run_model([t1, '_baseline'])
+
+%run_model([t1, '_nosectoral'])
+
+%run_model([t1, '_kappa1972'])
+
+%run_model([t1, '_kappa1972_nosectoral'])
+
+%create_table(t1)
